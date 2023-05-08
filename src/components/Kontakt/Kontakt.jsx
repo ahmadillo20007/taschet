44fde002ec6554564afd75594e9bda8c382f2213
habibullo071 <habibulloqodirov2006@gmail.com>
2023-05-08 15:22:50 +0500

    wagf

--- src/components/Kontakt/Kontakt.jsx
+++ src/components/Kontakt/Kontakt.jsx
@@ -57,7 +57,7 @@ function Kontakt() {
             </div>
           </div>
           <div className="right">
-
+  
           </div>
         </div>
       </div>
--- src/components/Servis/Servis.jsx
+++ src/components/Servis/Servis.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import './Servis.scss'
-import img26 from '../../img/sobirjon.png'
+import i1 from '../../img/1.png'
+import i2 from '../../img/2.png'
+import i3 from '../../img/3.png'
+import i4 from '../../img/4.png'
+import i5 from '../../img/5.png'
+import i6 from '../../img/6.png'
+import i7 from '../../img/7.png'
+import i8 from '../../img/8.png'
+import i9 from '../../img/9.png'
+import i10 from '../../img/10.png'
+import i11 from '../../img/11.png'
+import i12 from '../../img/12.png'
+import i13 from '../../img/13.png'
+import i14 from '../../img/14.png'
+import i15 from '../../img/15.png'
 function Servis() {
   return (
 <>
@@ -11,27 +25,27 @@ function Servis() {
             <p className='servis__item'>Sehr günstige Lage der Autowerkstatt mit sehr guter <br /> Verkehrsanbindung in Gelsenkirchen Buer</p>
             <div className="serviss">
               <ul className="left">
-<li className='left__item'> <img src={img26} alt="" /> Service Inspektionen an Ihren   Fahrzeug nach Herstellervorgaben    </li>
-<li className='left__item'>Auspuff </li>
-<li className='left__item'>Schaltgetriebeinstandsetzung</li>
-<li className='left__item'>Autoglas</li>
-<li className='left__item'>computerdiagnose </li>
+<li className='left__item'> <img src={i1} alt="" /> <p className='servis__list__item'>Service Inspektionen an Ihren   Fahrzeug nach Herstellervorgaben  </p>  </li>
+<li className='left__item'> <img src={i2} alt="" /> <p className='servis__list__item'>Auspuff</p> </li>
+<li className='left__item'> <img src={i3} alt="" /> <p className='servis__list__item'>Schaltgetriebeinstandsetzung</p></li>
+<li className='left__item'> <img src={i4} alt="" /> <p className='servis__list__item'>Autoglas</p></li>
+<li className='left__item'> <img src={i5} alt="" /> <p className='servis__list__item'>computerdiagnose</p> </li>
               </ul>
               <ul className="center">
              
-<li className='center__item'>reifenservice</li>
-<li className='center__item'>  Karosseriearbeiten</li>
-<li className='center__item'>Einbau von Anhängerkupplung</li>
-<li className='center__item'>Lackierarbeiten</li>
-<li className='center__item'>Bremsen</li>
+<li className='center__item'> <img src={i6} alt="" /> <p className='servis__list__item'>reifenservice</p></li>
+<li className='center__item'>  <img src={i7} alt="" />  <p className='servis__list__item'>Karosseriearbeiten</p></li>
+<li className='center__item'> <img src={i8} alt="" /> <p className='servis__list__item'>Einbau von Anhängerkupplung</p></li>
+<li className='center__item'> <img src={i9} alt="" /> <p className='servis__list__item'>Lackierarbeiten</p></li>
+<li className='center__item'> <img src={i10} alt="" /> <p className='servis__list__item'>Bremsen</p></li>
               </ul>
               <ul className="right">
         
-<li className='right__item'>automatikgertriebspülung </li>
-<li className='right__item'>Schlüssel Programmierung</li>
-<li className='right__item'>Kupplung</li>
-<li className='right__item'>Klimaservice</li>
-<li className='right__item'> sowie Abgasuntersuchung (DEKRA Stützpunkt)</li>Hauptuntersuchung
+<li className='right__item'><img src={i11} alt="" /> <p className='servis__list__item'>automatikgertriebspülung</p> </li>
+<li className='right__item'><img src={i12} alt="" /> <p className='servis__list__item'>Schlüssel Programmierung</p></li>
+<li className='right__item'><img src={i13} alt="" /> <p className='servis__list__item'>Kupplung</p></li>
+<li className='right__item'><img src={i14} alt="" /> <p className='servis__list__item'>Klimaservice</p></li>
+<li className='right__item'> <img src={i15} alt="" /> <p className='servis__list__item'>sowie Abgasuntersuchung (DEKRA Stützpunkt)</p></li>
               </ul>
             </div>
             <button className='servis__btn'>
